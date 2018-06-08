f1748c5c74df96aec851d69fe2d34d07a231214d
Werner <verwey.werner@gmail.com>
2018-06-08 21:28:00 +0200

    Corrected syntax error in regex

--- commands/raid.js
+++ commands/raid.js
@@ -451,8 +451,8 @@ class RaidSetup {
         .then(function (message) {
             if (reactions.length >0) {
                 for (var i = reactions.length - 1; i >= 0; i--) {
-                    var regex /<:[a-z]*:([0-9]*)>/g;
-                    var emojiarray = reactions[i].match(regex);
+                    var regex = /<:[a-z]*:([0-9]*)>/g;
+                    var emojiarray = regex.exec(reactions[i]);
                     let emoji = message.guild.emojis.get(emojiarray[0] || '448601060681187338');
                     message.react(emoji);
                 }
