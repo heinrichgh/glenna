49b5c88f5850a6e3084cf1308dd1d6f06b988d92
Werner <verwey.werner@gmail.com>
2018-06-10 00:16:29 +0200

    Setup command now functional

--- commands/setup.js
+++ commands/setup.js
@@ -27,22 +27,47 @@ class GuildSetup {
     }
 
     async getGuild(account, guild) {
-        let [rows] = await sql.execute(`SELECT name FROM guild WHERE guild_api_id = ?`, guild.id);
-        
-        if (rows.length = 1) {
-            return `${guild.name} [${guild.tag}]\n
-            Ranks: (todo)\n
-            Members: (todo)`;
+        let [rows] = await sql.execute('SELECT id FROM guild WHERE guild_api_id = ?', [guild.id]);
+        if (rows.length > 0) {
+            let sql_count_ranks = await sql.execute('SELECT COUNT(`id`) as c FROM `guild_rank` WHERE `guild_id` = (SELECT id FROM guild WHERE guild_api_id = ?)',[guild.id]);
+            let sql_count_members = await sql.execute('SELECT COUNT(`id`) as c FROM `guild_member` WHERE `guild_id` = (SELECT id FROM guild WHERE guild_api_id = ?)',[guild.id]);
+            return `Created guild: ${guild.name} [${guild.tag}]\nRanks: ${sql_count_ranks[0][0].c}\nMembers: ${sql_count_members[0][0].c}`;
         }
-        this.createGuild(guild);
-        return getGuild(guild);
+        await this.createGuild(account, guild);
+        return this.getGuild(account, guild);
+    }
+
+    async removeGuild(guild) {
+        let [rows] = await sql.execute('SELECT id FROM guild WHERE guild_api_id = ?', [guild.id]);
+        for (var i = rows.length - 1; i >= 0; i--) {
+            console.log(`Removing guild with ID: ${rows[i].id}`);
+            await sql.execute('DELETE FROM guild WHERE id = ?', [rows[i].id]);
+            await sql.execute('DELETE FROM guild_rank WHERE guild_id = ?', [rows[i].id]);
+            await sql.execute('DELETE FROM guild_member WHERE guild_id = ?', [rows[i].id]);
+        }
+
     }
 
     async createGuild(account, guild) {
-        await sql.execute(`DELETE FROM guild WHERE guild_api_id = ?`, guild.id);
-        var result = sql.execute(`INSERT INTO guild (id, guild_api_id, name, tag, leader) VALUES (NULL, '${guild.id}', '${guild.name}', '${guild.tag}', '${account.id}')`);
+        // insert guild
+        let sql_result_guild = await sql.execute('INSERT INTO guild (id, guild_api_id, name, tag, leader) VALUES (?, ?, ?, ?, ?)',[null, guild.id, guild.name, guild.tag, account.id]);
+
         // insert ranks
+        let gw2_api = new gwAPI(this.args);
+        let ranks = await gw2_api.guild_ranks_lookup(guild.id);
+        for (var i = ranks.length - 1; i >= 0; i--) {
+            let sql_result_ranks = await sql.execute('INSERT INTO guild_rank (id, rank, rank_order, guild_id) VALUES (?, ?, ?, ?)',[null, ranks[i].id, ranks[i].order, sql_result_guild[0].insertId]);
+        }
+        
         // insett members
+        let members = await gw2_api.guild_members_lookup(guild.id);
+        for (var i = members.length - 1; i >= 0; i--) {
+            if (members[i].rank.trim() != "invited")
+            {
+                let [rows] = await sql.execute('SELECT * FROM `guild_rank` WHERE rank LIKE ?',[members[i].rank]);
+                let sql_result_member = await sql.execute('INSERT INTO guild_member (id, guild_id, guild_member_name, discord_member_name, rank_id) VALUES (?, ?, ?, ?, ?)',[null, sql_result_guild[0].insertId, members[i].name, "", rows[0].id]);
+            }
+        }
     }
 
     async run() {
@@ -54,33 +79,39 @@ class GuildSetup {
         try {
 
             let gw2_api = new gwAPI(this.args);
-            let account = gw2_api.account_lookup();
-            console.log(account)
-            // var msg = "";
-            //
-            // for (var i = account.guild_leader.length - 1; i >= 0; i--) {
-            //     var guild_info = gw2_api.guild_lookup(account.guild_leader[i]);
-            //     msg += `${i} - ${guild_info.name} [${guild_info.tag}]\n`;
-            // }
-            //
-            // if (guilds.length > 0) {
-            //
-            //     this.message.reply(msg)
-            //     const filter = m => m.member === this.message.member;
-            //     let collected = await this.message.channel.awaitMessages(filter, {max: 1, time: 30000});
-            //
-            //     if (!collected.size) {
-            //         this.message.reply(`please respond with 1-${guilds.length}`);
-            //         return;
-            //     }
-            //
-            //     let num = collected.first().content.trim();
-            //     if (num > 0 && num <=(guilds.length) ) {
-            //         this.message.reply(getGuild(account, guilds[num]));
-            //     }
-            //     else { this.message.reply(`please respond with 1-${guilds.length}`); }
-            // }
-            // else { this.message.reply("not a leader"); }
+            let account = await gw2_api.account_lookup();
+            var msg = "";
+            var guilds = [];
+
+            if (account.guild_leader.length)
+            {
+                for (var i = account.guild_leader.length - 1; i >= 0; i--) {
+                    var guild_info = await gw2_api.guild_lookup(account.guild_leader[i]);
+                    msg += `${i+1} - ${guild_info.name} [${guild_info.tag}]\n`;
+                    guilds.push(guild_info);
+                }
+                msg += "Which guild you would like to add?";
+
+                if (guilds.length > 0) {
+                
+                    this.message.reply(msg)
+                    const filter = m => m.member === this.message.member;
+                    let collected = await this.message.channel.awaitMessages(filter, {max: 1, time: 30000});
+                
+                    if (!collected.size) {
+                        this.message.reply(`please respond with 1-${guilds.length}`);
+                        return;
+                    }
+                
+                    let num = collected.first().content.trim();
+                    if (num > 0 && num <=(guilds.length) ) {
+                        await this.removeGuild(guilds[num-1]);
+                        this.message.reply(await this.getGuild(account, guilds[num-1]));
+                    }
+                    else { this.message.reply(`please respond with 1-${guilds.length}`); }
+                }
+                else { this.message.reply("not a leader"); }
+            }
         } catch (e) {
             console.error(e);
         }
@@ -89,10 +120,12 @@ class GuildSetup {
 }
 
 exports.run = (client, message, args) => {
-    // var pattern = /[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*/g;
-    // message.reply("Not Implemented! Demo Only")
-    let guildSetup = new GuildSetup(client, message, args);
-    guildSetup.run().catch(console.error);
+    var pattern = /[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*/g;
+    if (pattern.test(args)) {
+        let guildSetup = new GuildSetup(client, message, args);
+        guildSetup.run().catch(console.error);    
+    }
+    
 };
 
 
