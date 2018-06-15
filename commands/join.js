0616e847dd20d3d59bf182fec93a9030c891f3ad
Werner <verwey.werner@gmail.com>
2018-06-15 21:12:01 +0200

    Added support for non-members

--- commands/join.js
+++ commands/join.js
@@ -30,38 +30,25 @@ class AccountSetup {
                 this.message.reply(`Hi ${guild[0][0].guild_member_name} your rank has been set to '${guild[0][0].rank}'`);
             }
             else {
-                console.log("role does not exist, perhaps rerun !setup?")
-            }
-            // if (account.guild_leader.length)
-            // {
-            //     for (var i = account.guild_leader.length - 1; i >= 0; i--) {
-            //         var guild_info = await gw2_api.guild_lookup(account.guild_leader[i]);
-            //         msg += `${i+1} - ${guild_info.name} [${guild_info.tag}]\n`;
-            //         guilds.push(guild_info);
-            //     }
-            //     msg += "Which guild you would like to add?";
+                this.message.reply("User not found in <guildnamehere>, would you like to join as <friend>");
+                let collected = await this.message.channel.awaitMessages(filter, {max: 1, time: 30000});
 
-            //     if (guilds.length > 0) {
-                
-            //         this.message.reply(msg)
-            //         const filter = m => m.member === this.message.member;
-            //         let collected = await this.message.channel.awaitMessages(filter, {max: 1, time: 30000});
-                
-            //         if (!collected.size) {
-            //             this.message.reply(`please respond with 1-${guilds.length}`);
-            //             return;
-            //         }
-                
-            //         let num = collected.first().content.trim();
-            //         if (num > 0 && num <=(guilds.length) ) {
-            //             await this.removeGuild(guilds[num-1]);
-            //             this.message.reply(await this.getGuild(account, guilds[num-1]));
-            //             await this.createRanks(guilds[num-1]);
-            //         }
-            //         else { this.message.reply(`please respond with 1-${guilds.length}`); }
-            //     }
-            //     else { this.message.reply("not a leader"); }
-            // }
+                if (!collected.size) {
+                    this.message.reply(`No response received`);
+                    return;
+                }
+                if (collected.first().content.trim() == "yes")
+                {
+                    const friendRole = this.message.guild.roles.find("name", "Friend Of Flux");
+                    if (!friendRole) {
+                        this.message.guild.createRole({
+                            name: "Friend Of Flux"
+                        })
+                        .then(role => console.log(`Created new role with name ${role.name}.`))
+                        .catch(console.error)
+                    }
+                }
+            }
         } catch (e) {
             console.error(e);
         }
@@ -70,7 +57,6 @@ class AccountSetup {
 }
 
 exports.run = (client, message, args) => {
-    message.reply("Not Implemented! Demo Only");
     var pattern = /[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*/g;
     if (pattern.test(args)) {
         let accountSetup = new AccountSetup(client, message, args);
