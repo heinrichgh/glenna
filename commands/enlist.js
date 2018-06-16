73320ea6f8d73a3b53807fc3b2858396144049d9
Werner <verwey.werner@gmail.com>
2018-06-17 01:53:55 +0200

    update test code

--- commands/enlist.js
+++ commands/enlist.js
@@ -174,11 +174,15 @@ class Enlist {
             const filter = (reaction, user) => user.id === this.message.member.id;
             const collector = message.createReactionCollector(filter, { time: 15000 });
             collector.on('collect', (r) => {
-                let spot = await sql.execute('SELECT raid_squad.spot, profession.title FROM `raid_squad` LEFT JOIN raid_squad_restriction on raid_squad.id = raid_squad_restriction.raid_squad_id JOIN profession ON raid_squad_restriction.profession_id = profession.id WHERE raid_squad.raid_id = ? AND profession.title LIKE ?',[raid.id, r.emoji.name]);
-                if (spot[0]) {
-                    console.log(spot[0]);
-                    await sql.execute('UPDATE `raid_squad` SET `user_id` = ? WHERE `raid_squad`.`raid_id`  = ? AND `raid_squad`.`spot` = ?',[this.message.member.id, raid.id, spot]);    
-                }
+                
+                let [roles] = await sql.execute('SELECT raid_role.title FROM raid_role JOIN raid_squad_restriction ON raid_role.id = raid_squad_restriction.raid_role_id JOIN raid_squad ON raid_squad.id = raid_squad_restriction.raid_squad_id JOIN profession ON raid_squad_restriction.profession_id = profession.id WHERE raid_squad.raid_id = ? AND profession.title LIKE ?',[raid.id, r.emoji.name]);
+                
+                console.log(roles);
+                //let spot = await sql.execute('SELECT raid_squad.spot, profession.title FROM `raid_squad` LEFT JOIN raid_squad_restriction on raid_squad.id = raid_squad_restriction.raid_squad_id JOIN profession ON raid_squad_restriction.profession_id = profession.id WHERE raid_squad.raid_id = ? AND profession.title LIKE ?',[raid.id, r.emoji.name]);
+                // if (spot[0]) {
+                //     console.log(spot[0]);
+                //     //await sql.execute('UPDATE `raid_squad` SET `user_id` = ? WHERE `raid_squad`.`raid_id`  = ? AND `raid_squad`.`spot` = ?',[this.message.member.id, raid.id, spot]);    
+                // }
                 
                 this.message.reply(`Enlisted as ${r.emoji.name}`);
                 message.delete();
