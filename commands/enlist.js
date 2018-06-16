07314e860d9a36533c401383853061a44164f6b3
Werner <verwey.werner@gmail.com>
2018-06-16 15:50:55 +0200

    Remove test code

--- commands/enlist.js
+++ commands/enlist.js
@@ -203,11 +203,6 @@ class Enlist {
             if (raid.length) {
                 await this.sendSummary(raid[0], this.message.channel);    
             }
-            const emojiList = this.message.guild.emojis.map(e=>e.toString()).join(" ");
-            console.log(emojiList);
-            this.message.channel.send(emojiList);
-
-
         } catch (e) {
             console.error(e);
         }
