d59b46f7c5774d45076a9c98d4d9c2c66bf2ec25
Werner <verwey.werner@gmail.com>
2018-06-29 17:32:02 +0200

    Remove reaction watch code

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -3,7 +3,7 @@ const config = require("../config.js");
 exports.run = async (client, messageReaction, user) => {
 	if (user.bot) return;
 
-	// Handle raid channel
-	if (messageReaction.message.channel.id == config.raidChannelId)
-		messageReaction.message.channel.send(`User <@${user.id}> withdrew as ${messageReaction.emoji}`).catch(console.error);
+	// // Handle raid channel
+	// if (messageReaction.message.channel.id == config.raidChannelId)
+	// 	messageReaction.message.channel.send(`User <@${user.id}> withdrew as ${messageReaction.emoji}`).catch(console.error);
 }
