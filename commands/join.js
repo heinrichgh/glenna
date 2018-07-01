d122c4b9eff7b32aca17a468c59c3b34f3aff314
Werner <verwey.werner@gmail.com>
2018-07-01 20:15:54 +0200

    test

--- commands/join.js
+++ commands/join.js
@@ -29,6 +29,7 @@ class AccountSetup {
             }
             else {
                 this.message.reply("User not found in <guildnamehere>, would you like to join as <friend>");
+                const filter = m => m.member === this.message.member;
                 let collected = await this.message.channel.awaitMessages(filter, {max: 1, time: 30000});
 
                 if (!collected.size) {
