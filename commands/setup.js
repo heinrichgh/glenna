abcfbddbfcc05a908a99bfb3c33e109b6555f0bf
Werner <verwey.werner@gmail.com>
2018-07-01 20:02:52 +0200

    test

--- commands/setup.js
+++ commands/setup.js
@@ -62,11 +62,13 @@ class GuildSetup {
 
         let [sql_result_guild] = await sql.execute('SELECT id FROM `guild` WHERE `guild_api_id` = ?',[guild.id]);
         if (sql_result_guild[0]) {
-            console.log(sql_result_guild);
+            console.log('found');
             await sql.execute('UPDATE `guild` SET guild_api_id = ?, name = ?, tag = ?, leader = ? WHERE id = ?',[guild.id, guild.name, guild.tag, account.id, sql_result_guild[0].id]);
         }
-        else
+        else {
+            console.log('not found')
             [sql_result_guild] = await sql.execute('INSERT INTO guild (id, guild_api_id, name, tag, leader) VALUES (?, ?, ?, ?, ?)',[null, guild.id, guild.name, guild.tag, account.id]);
+        }
         
 
         // insert ranks
